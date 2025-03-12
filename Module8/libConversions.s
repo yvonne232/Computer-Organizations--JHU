9ac36839f516df10cc2be888df5d95f413f99140
yvonne <zlin80@jh.edu>
2025-03-12 23:06:30 +0000

    update

--- Module8/libConversions.s
+++ Module8/libConversions.s
@@ -1,5 +1,6 @@
 .global miles2kilometer
 .global kph
+.global CToF
 
 .text
 miles2kilometer:
@@ -10,9 +11,9 @@ miles2kilometer:
 	
 	# r0 = r0 * 161 / 100
 	# Since 1.61 is float, we need special registers for float number and this avoids that
-	LDR r1, =161
+	MOV r1, #161
 	MUL r0, r0, r1
-	LDR r1, =100
+	MOV r1, #100
 	BL __aeabi_idiv
 
 	# Pop stack
@@ -25,7 +26,7 @@ miles2kilometer:
 .text
 kph:
 	# kph(int hours, int miles)
-	# Push tack
+	# Push stack
 	SUB sp, sp, #4
 	STR lr, [sp]
 
@@ -47,6 +48,28 @@ kph:
 .data
 # End kph
 	
-
+.text
+CToF:
+	#CToF (Celsius to Fahrenheit)	
+	# Push stack
+	SUB sp, sp, #4
+	STR lr, [sp]
 	
-
+	# F = (C * 9 / 5) + 32
+	MOV r1, #9
+	# r0 = r0 * 9
+	MUL r0, r0, r1
+	MOV r1, #5
+	# r0 = r0/5
+	BL __aeabi_idiv
+	MOV r1, #32
+	# r0 = r0 + 32
+	ADD r0, r0, r1
+	
+	# Pop stacl
+	LDR lr, [sp]
+	ADD sp, sp, #4
+	MOV pc, lr
+.data
+# End CToF
+	 
--- Module8/main.s
+++ Module8/main.s
@@ -5,7 +5,7 @@ main:
 	SUB sp, sp, #4
 	STR lr, [sp]
 	
-	# Call Function: miles2kilometer
+	# ---Call Function: miles2kilometer---
 	# Prompt for and read input
 	LDR r0, =prompt_miles
 	BL printf
@@ -17,7 +17,7 @@ main:
 	LDR r4, =mile
 	LDR r4, [r4]
 	
-	# Call mile2kilometer function
+	# Call miles2kilometer function
 	MOV r0, r4
 	BL miles2kilometer
 	
@@ -25,8 +25,11 @@ main:
 	MOV r1, r0
 	LDR r0, =output_km
 	BL printf
+
+	# Move kilometer to r4
+	MOV r4, r1
 	
-	# Call Function: kph
+	# ---Call Function: kph---
 	# Prompt for and read input
 	LDR r0, =prompt_hours
 	BL printf
@@ -45,7 +48,24 @@ main:
 	# Print answer
 	MOV r1, r0
 	LDR r0, =output_kph
-	BL printf	
+	BL printf
+
+	#---Call Function: CToF---
+	# Prompt for and read input
+	LDR r0, =prompt_celsius
+	BL printf
+	LDR r0, =format_celsius
+	LDR r1, =celsius
+	BL scanf
+
+	# Call CToF function
+	BL CToF
+	MOV r6, r0
+
+	MOV r1, r6
+	LDR r0, =output_fahrenheit
+	BL printf
+		
 	
 	# Pop stack
 	LDR lr, [sp]
@@ -54,7 +74,7 @@ main:
 
 	
 .data
-	prompt_miles: .asciz "Enter miles: "
+	prompt_miles: .asciz "Enter speed in miles: "
 	format_miles: .asciz "%d"
 	mile: .word 0
 	output_km: .asciz "Speed in kilometer: %d\n "
@@ -63,5 +83,10 @@ main:
 	format_hours: .asciz "%d"
 	hours: .word 0
 	output_kph: .asciz "Kilometers per hour: %d\n"
+
+	prompt_celsius: .asciz "Enter temperature in Celsius: "
+	format_celsius: .asciz "%d"
+	celsius: .word 0
+	output_fahrenheit: .asciz "Temperature in Fahrenheit: %d\n"
 	
 	
