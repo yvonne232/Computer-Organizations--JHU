455dd26884317096a408d5c15b2f391bfcc1acc6
yvonne <zlin80@jh.edu>
2025-03-12 03:40:41 +0000

    Module8/main.s

--- Module8/main.s
+++ Module8/main.s
@@ -14,7 +14,8 @@ main:
 	LDR r1, =mile
 	BL scanf
 	# Move miles to r4
-	LDR r4, r0
+	LDR r4, =mile
+	LDR r4, [r4]
 	
 	# Call mile2kilometer function
 	MOV r0, r4
@@ -26,6 +27,25 @@ main:
 	BL printf
 	
 	# Call Function: kph
+	# Prompt for and read input
+	LDR r0, =prompt_hours
+	BL printf
+	LDR r0, =format_hours
+	LDR r1, =hours
+	BL scanf
+	# Move hours to r5
+	LDR r5, =hours
+	LDR r5, [r5]
+
+	# R0: hours, R1: miles; call kph function
+	MOV r0, r5
+	MOV r1, r4
+	BL kph
+	
+	# Print answer
+	MOV r1, r0
+	LDR r0, =output_kph
+	BL printf	
 	
 	# Pop stack
 	LDR lr, [sp]
@@ -42,5 +62,6 @@ main:
 	prompt_hours: .asciz "Enter hours: "
 	format_hours: .asciz "%d"
 	hours: .word 0
+	output_kph: .asciz "Kilometers per hour: %d\n"
 	
 	
