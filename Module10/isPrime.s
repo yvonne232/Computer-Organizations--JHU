3b08062bda7ed787a54c66412c1ff556b6c1bf07
yvonne <zlin80@jh.edu>
2025-04-01 22:42:56 +0000

    Update

--- Module10/isPrime.s
+++ Module10/isPrime.s
@@ -5,7 +5,7 @@ main:
 	# Program dictionary
 	# r4 - save user input number
 
-	SUB sp, sp, #4
+	SUB sp, sp, #8
 	STR lr, [sp, #0]
 	STR r4, [sp, #4]
 
@@ -23,39 +23,39 @@ main:
 		CMP r4, #-1
 		BEQ endSentinelLoop
 
-		# Else, get the next user input, and continue the sentinel loop 
-		LDR r0, =promptMsg
-		BL printf
-		LDR r0, =scanFormat
-		LDR r1, =number
-		BL scanf
-		B startSentinelLoop
-
 		# Error checking: if input is 0, 1, 2 or any negative number other than -1 are entered, print the error message
-		CMP r4, #2
-		BLE InvalidInput
+		CMP r4, #3
+		BLT InvalidInput
 
+		# Else, check if the input is a prime number
+
+
+		# Print out the error message and get next valid input
 		InvalidInput: 
 			LDR r0, =errorMsg
 			BL printf
-			B startSentinelLoop
-		
+			B getNextInput
 
-		# Initialize checking prime loop
-		
-			
+		# get the next user input, and continue the sentinel loop 
+		getNextInput:
+			LDR r0, =promptMsg
+			BL printf
+			LDR r0, =scanFormat
+			LDR r1, =number
+			BL scanf
+			B startSentinelLoop
 
 	endSentinelLoop:
 		# End the sentinel loop if user input is -1
 		# Pop stack
 		LDR lr, [sp, #0]
 		LDR r4, [sp, #4]
-		ADD sp, sp, #4
+		ADD sp, sp, #8
 		MOV pc, lr 
 	
 
 .data
-promptMsg: .asciz "Enter a number: (-1 to quit)\n"
+promptMsg: .asciz "\nEnter a number: (-1 to quit)\n"
 number: .word 0
 scanFormat: .asciz "%d"
 errorMsg: .asciz "Error: Input must be >2 or -1 to quit.\n"
